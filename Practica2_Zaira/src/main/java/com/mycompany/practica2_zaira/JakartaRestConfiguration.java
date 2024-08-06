4caa4ed044e6713ce57ee504de89c9bc2f626f62
zairaescobar <zescobar@itspa.edu.mx>
2024-08-06 12:50:02 -0600

    inicio de repositorio final del curso jakarta

---
+++ Practica2_Zaira/src/main/java/com/mycompany/practica2_zaira/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.practica2_zaira;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
