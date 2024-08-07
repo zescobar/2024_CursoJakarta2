d040d0f6b016b40fdea6660d182c76643077efbc
zairaescobar <zescobar@itspa.edu.mx>
2024-08-07 11:50:02 -0600

    se agregó Formulario y Validaciones

---
+++ Practica2_Zaira/src/main/java/com/mycompany/practica2_zaira/validations/EmailAddressValidator.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica2_zaira.validations;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.component.html.HtmlInputText;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.FacesValidator;
+import jakarta.faces.validator.Validator;
+import jakarta.faces.validator.ValidatorException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.validator.routines.EmailValidator;
+
+@FacesValidator(value = "emailValidator")
+public class EmailAddressValidator implements Validator {
+  @Override
+  public void validate(FacesContext facesContext, UIComponent uiComponent,
+          Object value) throws ValidatorException {
+
+    EmailValidator emailValidator = EmailValidator.getInstance();
+    HtmlInputText htmlInputText = (HtmlInputText) uiComponent;
+    String emailAddress = (String) value;
+    if (!StringUtils.isEmpty(emailAddress)) {
+      if (!emailValidator.isValid(emailAddress)) {
+        FacesMessage facesMessage = new FacesMessage(
+                htmlInputText.getLabel() + ": email format is not valid");
+        throw new ValidatorException(facesMessage);
+      }
+    }
+  }
+}
